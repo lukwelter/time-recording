706a9a870d2ba24f46a53d48afaddf8a61957d3b
lukwelter <lukaswelter@icloud.com>
2026-03-23 18:32:12 +0100

    Delete src directory

--- src/components/ui/separator.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { cn } from "@/lib/utils";
-
-export function Separator({ className = "", ...props }) {
-  return <div className={cn("h-px w-full bg-slate-200", className)} {...props} />;
-}
